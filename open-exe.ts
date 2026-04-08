c368f008c783c75d95f1f5e5050bc057bd8da578
moshoss <tyd63er@gmail.com>
2026-04-08 11:58:34 +0800

    Update open-exe.ts

--- open-exe.ts
+++ open-exe.ts
@@ -14,3 +14,48 @@ function openExe(appPath) {
   })
 }
 
+function isProcessRunning(pid) {
+  try {
+    process.kill(pid, 0);
+    return true;
+  } catch (error) {
+    if (error.code === 'ESRCH') {
+      return false;
+    }
+    if (error.code === 'EPERM') {
+      return true;
+    }
+    return false;
+  }
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function checkPidKilled(pid, timeout = 30000) {
+  let prevTime = Date.now()
+  return new Promise((resolve) => {
+    const loop = async () => {
+      if (Date.now() - prevTime > timeout) {
+        resolve(false)
+        return
+      }
+      await sleep(1000)
+      if (!isProcessRunning(pid)) {
+        resolve(true)
+      } else {
+        loop()
+      }
+    }
+    loop()
+  })
+}
+
+
+const foo = async () => {
+  const killed = await checkPidKilled(4312)
+  console.log('killed', killed)
+}
+
+foo()
